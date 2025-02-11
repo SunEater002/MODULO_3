b0445c9849785615fb276d14ec10567fcc58d54a
omar <omar.terwogt18@gmail.com>
2025-02-10 21:20:24 -0600

    tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,26 @@
+console.log(typeof 42); 
+// number
+console.log(typeof 'Hola');
+// string
+console.log(typeof 'veinticico');
+//string
+console.log(typeof -666);
+// number
+console.log(typeof true);
+// boolean
+console.log(typeof 0);
+// number
+console.log(typeof "");
+// string
+console.log(typeof null);
+// object
+console.log(typeof undefined);
+// undefined
+console.log(typeof FALSE);
+// undefined
+console.log(typeof TRUE);
+// undefined
+console.log(typeof NaN);
+// number
+console.log(typeof "Hola Mundo");
+// string
